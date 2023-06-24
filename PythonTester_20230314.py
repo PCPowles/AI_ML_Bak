767f825aa2b064cd574854ec6355f625dccca671
PCPowles <45850239+PCPowles@users.noreply.github.com>
2023-06-24 15:04:10 -0400

    Add files via upload

---
+++ PythonTester_20230314.py
@@ -0,0 +1,8 @@
+for x in range(10):
+    if (x % 2 == 0):
+        print(str(x) + " Even") 
+    elif (x % 2 != 0):
+        print(str(x) + " Odd")
+    else:
+        break
+        print ("Break on: " + x)
